200dbd9b9c603211d54f6cb154aa54ec3e28ba3b
Lakshman95 <67822573+Lakshman95@users.noreply.github.com>
2020-07-04 18:28:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,103 +1,83 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  4 14:18:30 2020
+import random
 
-@author: 690235
-"""
-class QueenChessBoard:
-    def __init__(self, size):
-        # board has dimensions size x size
-        self.size = size
-        # columns[r] is a number c if a queen is placed at row r and column c.
-        # columns[r] is out of range if no queen is place in row r.
-        # Thus after all queens are placed, they will be at positions
-        # (columns[0], 0), (columns[1], 1), ... (columns[size - 1], size - 1)
-        self.columns = []
- 
-    def place_in_next_row(self, column):
-        self.columns.append(column)
- 
-    def remove_in_current_row(self):
-        return self.columns.pop()
- 
-    def is_this_column_safe_in_next_row(self, column):
-        # index of next row
-        row = len(self.columns)
- 
-        # check column
-        for queen_column in self.columns:
-            if column == queen_column:
-                return False
- 
-        # check diagonal
-        for queen_row, queen_column in enumerate(self.columns):
-            if queen_column - queen_row == column - row:
-                return False
- 
-        # check other diagonal
-        for queen_row, queen_column in enumerate(self.columns):
-            if ((self.size - queen_column) - queen_row
-                == (self.size - column) - row):
-                return False
- 
-        return True
- 
-    def display(self):
-        for row in range(self.size):
-            for column in range(self.size):
-                if column == self.columns[row]:
-                    print(column, end=' ')
+def random_chromosome(size): #making random chromosomes 
+    return [ random.randint(1, nq) for _ in range(nq) ]
 
- 
-def solve_queen(size):
-    """Display a chessboard for each possible configuration of placing n queens
-    on an n x n chessboard and print the number of such configurations."""
-    board = QueenChessBoard(size)
-    number_of_solutions = 0
- 
-    row = 0
-    column = 0
-    # iterate over rows of board
-    while number_of_solutions == 0:
-        # place queen in next row
-        while column < size:
-            if board.is_this_column_safe_in_next_row(column):
-                board.place_in_next_row(column)
-                row += 1
-                column = 0
-                break
-            else:
-                column += 1
- 
-        # if could not find column to place in or if board is full
-        if (column == size or row == size):
-            # if board is full, we have a solution
-            if row == size:
-                board.display()
-                print()
-                number_of_solutions += 1
- 
-                # small optimization:
-                # In a board that already has queens placed in all rows except
-                # the last, we know there can only be at most one position in
-                # the last row where a queen can be placed. In this case, there
-                # is a valid position in the last row. Thus we can backtrack two
-                # times to reach the second last row.
-                board.remove_in_current_row()
-                row -= 1
- 
-            # now backtrack
-            try:
-                prev_column = board.remove_in_current_row()
-            except IndexError:
-                # all queens removed
-                # thus no more possible configurations
-                break
-            # try previous row again
-            row -= 1
-            # start checking at column = (1 + value of column in previous row)
-            column = 1 + prev_column
- 
- 
-n = 8
-solve_queen(n)
+def fitness(chromosome):
+    horizontal_collisions = sum([chromosome.count(queen)-1 for queen in chromosome])/2
+    diagonal_collisions = 0
+
+    n = len(chromosome)
+    left_diagonal = [0] * 2*n
+    right_diagonal = [0] * 2*n
+    for i in range(n):
+        left_diagonal[i + chromosome[i] - 1] += 1
+        right_diagonal[len(chromosome) - i + chromosome[i] - 2] += 1
+
+    diagonal_collisions = 0
+    for i in range(2*n-1):
+        counter = 0
+        if left_diagonal[i] > 1:
+            counter += left_diagonal[i]-1
+        if right_diagonal[i] > 1:
+            counter += right_diagonal[i]-1
+        diagonal_collisions += counter / (n-abs(i-n+1))
+    
+    return int(maxFitness - (horizontal_collisions + diagonal_collisions)) #28-(2+3)=23
+
+def probability(chromosome, fitness):
+    return fitness(chromosome) / maxFitness
+
+def random_pick(population, probabilities):
+    populationWithProbabilty = zip(population, probabilities)
+    total = sum(w for c, w in populationWithProbabilty)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in zip(population, probabilities):
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False, "Shouldn't get here"
+        
+def reproduce(x, y): #doing cross_over between two chromosomes
+    n = len(x)
+    c = random.randint(0, n - 1)
+    return x[0:c] + y[c:n]
+
+def mutate(x):  #randomly changing the value of a random index of a chromosome
+    n = len(x)
+    c = random.randint(0, n - 1)
+    m = random.randint(1, n)
+    x[c] = m
+    return x
+
+def genetic_queen(population, fitness):
+    mutation_probability = 0.03
+    new_population = []
+    probabilities = [probability(n, fitness) for n in population]
+    for i in range(len(population)):
+        x = random_pick(population, probabilities) #best chromosome 1
+        y = random_pick(population, probabilities) #best chromosome 2
+        child = reproduce(x, y) #creating two new chromosomes from the best 2 chromosomes
+        if random.random() < mutation_probability:
+            child = mutate(child)
+        new_population.append(child)
+        if fitness(child) == maxFitness: break
+    return new_population
+
+if __name__ == "__main__":
+    nq = 8
+    maxFitness = (nq*(nq-1))/2  # 8*7/2 = 28
+    population = [random_chromosome(nq) for _ in range(100)]
+    
+    generation = 1
+
+    while not maxFitness in [fitness(chrom) for chrom in population]:
+        population = genetic_queen(population, fitness)
+        generation += 1
+    chrom_out = []
+    for chrom in population:
+        if fitness(chrom) == maxFitness:
+            print(chrom, end = " ")
+            for x in range(len(chrom)): 
+                print (chrom[x], end=" ")            
